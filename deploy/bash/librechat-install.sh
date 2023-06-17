b6a120855556783757b44912f6a67bbf00b56519
Mike 'MJ' Johnson <mikejohnsonit@gmail.com>
2023-06-17 22:56:26 +0000

    fix issue with docker gpg key

--- deploy/bash/librechat-install.sh
+++ deploy/bash/librechat-install.sh
@@ -18,7 +18,7 @@ echo -e "${GREEN}Installing Docker${NC}"
 sudo apt-get update || check_error "apt-get update"
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common || check_error "apt-get install"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || check_error "curl"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg.temp && sudo mv /usr/share/keyrings/docker-archive-keyring.gpg.temp /usr/share/keyrings/docker-archive-keyring.gpg || check_error "curl"
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || check_error "tee"
 sudo apt-get update || check_error "apt-get update"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin || check_error "apt-get install"

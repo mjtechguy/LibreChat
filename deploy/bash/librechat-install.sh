00ed69a879e3c755805f53071fbbc7e0da426ad5
Mike 'MJ' Johnson <mikejohnsonit@gmail.com>
2023-06-17 23:06:18 +0000

    fix exit state when updating group membership

--- deploy/bash/librechat-install.sh
+++ deploy/bash/librechat-install.sh
@@ -26,7 +26,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 # Add user to the docker group
 echo -e "${GREEN}Updating group membership${NC}"
 sudo usermod -aG docker $USER || check_error "usermod"
-newgrp docker
+exec su -l $USER
 
 # Prompt user for update confirmation
 echo -e "${GREEN}Update Confirmation${NC}"
